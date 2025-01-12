92a005432e09670a6dfba6b2d017e8b53993925d
Bryn Newell <41923284+bryn-newell@users.noreply.github.com>
2025-01-12 14:32:19 -0700

    🔧 hiding some vue errors in index and updating 11ty config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,7 @@
+/**
+ *  @param {import("@11ty/eleventy/src/UserConfig")} eleventyConfig
+ */
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./assets");
+  // eleventyConfig.addWatchTarget('./src/scss/**/*');
 };
