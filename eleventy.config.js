2bbcba4888a99a864d6a1207f2e42d0f6bd0fbfb
Bryn Newell <41923284+bryn-newell@users.noreply.github.com>
2025-02-18 14:08:40 -0700

    ⚙️ adding RSS feed and config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,27 @@
+import { feedPlugin } from "@11ty/eleventy-plugin-rss"
+
 /**
  *  @param {import("@11ty/eleventy/src/UserConfig")} eleventyConfig
  */
 export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(feedPlugin, {
+    type: "rss",
+    outputPath: "/feed.xml",
+    collection: {
+      name: "article",
+      limit: 0,
+    },
+    metadata: {
+      language: "en",
+      title: "Bryn Newell | Creative Developer",
+      subtitle: "The personal portfolio site of Creative Developer Bryn Newell",
+      base: "https://bryn.codes/",
+      author: {
+        name: "Bryn Newell",
+        email: "bryn.codes@gmail.com",
+      }
+    }
+  });
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addWatchTarget('./src/styles/');
 
