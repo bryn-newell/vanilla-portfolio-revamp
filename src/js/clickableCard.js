de50e94bf3bd2b979cecf0de78a88afbcf735b23
Bryn Newell <41923284+bryn-newell@users.noreply.github.com>
2025-02-19 15:49:43 -0700

    ⚙️ making the full article card clickable, adding js folder handling

---
+++ src/js/clickableCard.js
@@ -0,0 +1,25 @@
+// Code inspiration and concept from https://inclusive-components.design/cards/
+const clickableCard = () => {
+  const cards = document.querySelectorAll('.card__clickable')
+
+  cards.forEach(card => {
+    let down;
+    let up;
+    const link = card.querySelector('a');
+    card.onmousedown = () => { down = +new Date() };
+    card.onmouseup = () => {
+      up = +new Date();
+      if ((up - down) < 200) {
+        link.click();
+      }
+    }
+    // adding styles with JS because they're not necessary if the JS doesn't load
+    card.style.cursor = 'pointer';
+  });
+
+
+};
+
+window.onload = () => {
+  clickableCard();
+}
