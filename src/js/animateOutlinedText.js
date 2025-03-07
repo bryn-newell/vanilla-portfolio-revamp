146ce5ee69b6490dbab7906912b4802932da8bdb
Bryn Newell <41923284+bryn-newell@users.noreply.github.com>
2025-03-07 13:17:04 -0800

    🧈 adding the outine fill animation !!

---
+++ src/js/animateOutlinedText.js
@@ -0,0 +1,19 @@
+const ATTRIBUTE_NAME = "reveal";
+const DISTANCE_FROM_BOTTOM_TO_REVEAL = `200px`;
+
+const revealHandler = (entries, observer) => {
+  entries.forEach((entry) => {
+    console.log(entry, entry.isIntersecting, entry.target.hasAttribute(ATTRIBUTE_NAME))
+    if (entry.isIntersecting && entry.target.hasAttribute(ATTRIBUTE_NAME)) {
+      entry.target.classList.add('revealed');
+      // only reveal once
+      entry.target.removeAttribute(ATTRIBUTE_NAME);
+    }
+  });
+};
+
+const revealObserver = new IntersectionObserver(revealHandler, {
+  rootMargin: `0px 0px -${DISTANCE_FROM_BOTTOM_TO_REVEAL} 0px`
+});
+
+[...document.querySelectorAll(`[${ATTRIBUTE_NAME}]`)].forEach((el) => revealObserver.observe(el));
