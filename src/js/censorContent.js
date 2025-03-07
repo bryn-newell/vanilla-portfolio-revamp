00a74f85ddd77c8b90bfa9334b8525eef557c2b5
Bryn Newell <41923284+bryn-newell@users.noreply.github.com>
2025-03-07 13:16:43 -0800

    🐛  fixing censorship bug for other pages

--- src/js/censorContent.js
+++ src/js/censorContent.js
@@ -1,23 +1,26 @@
 const censorContent = () => {
-  document.getElementById('censorship-button').onclick = () => {
-    const swears = document.querySelectorAll('.swear');
-    const isCensored = swears?.[0].classList.contains('censored');
-    const censorSentence = document.querySelector('.censor-sentence');
+  const censorshipButton = document.getElementById('censorship-button');
+  if (censorshipButton) {
+    censorshipButton.onclick = () => {
+      const swears = document.querySelectorAll('.swear');
+      const isCensored = swears?.[0].classList.contains('censored');
+      const censorSentence = document.querySelector('.censor-sentence');
 
-    if (isCensored) {
-      // Remove the censoring on click
-      swears.forEach(swear => {
-        swear.classList.remove('censored')
-      })
-      censorSentence.innerText = 'If you’d like the censored version you can '
-    } else {
-      // Add the censoring on click
-      swears.forEach(swear => {
-        swear.classList.add('censored')
-      })
-      censorSentence.innerText = 'If you\'re fine with a little swearing you can '
-    }
-  };
+      if (isCensored) {
+        // Remove the censoring on click
+        swears.forEach(swear => {
+          swear.classList.remove('censored')
+        })
+        censorSentence.innerText = 'If you’d like the censored version you can '
+      } else {
+        // Add the censoring on click
+        swears.forEach(swear => {
+          swear.classList.add('censored')
+        })
+        censorSentence.innerText = 'If you\'re fine with a little swearing you can '
+      }
+    };
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
